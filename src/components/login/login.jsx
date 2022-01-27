d7dfc1b2403f8bf3faf73d9374a5fa0442d84f0e
Yujin Jang <jyooj08@gmail.com>
2022-01-27 23:38:51 +0900

    Add authentication service

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Header from '../header/header';
 import styles from './login.module.css';
 
-const Login = (props) => {
+const Login = ({authService}) => {
+    const navi = useNavigate();
+    
+    const onLogin = () => {
+        authService.login()
+        .then(result => {
+            const user = result.user;
+            console.log(user.displayName, user.uid);
+            navi('/main');
+        })
+    }
+
     return (<div className={styles.container}>
         <Header />
-        <button className={styles.loginBtn}>Google Login</button>
+        <button className={styles.loginBtn} onClick={onLogin}>Google Login</button>
     </div>)
 }
 
---
+++ src/services/AuthService.js
@@ -0,0 +1,33 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+class AuthService{
+    constructor(){
+        this.firebaseConfig = {
+            apiKey: "AIzaSyCDNu1QRNk27AhmH4E6-S-afBfyxWFh9_g",
+            authDomain: "haru-diary-bda69.firebaseapp.com",
+            projectId: "haru-diary-bda69",
+            storageBucket: "haru-diary-bda69.appspot.com",
+            messagingSenderId: "93188198611",
+            appId: "1:93188198611:web:23c0128d6ff394b593c426"
+          };
+
+          this.app = initializeApp(this.firebaseConfig);
+          this.googleProvider = new GoogleAuthProvider();
+          this.auth = getAuth();
+    }
+
+    print(){
+        console.log('hello authservice!');
+    }
+
+    login(){
+        return signInWithPopup(this.auth, this.googleProvider);
+    }
+
+    logout(){
+        return signOut(this.auth);
+    }
+}
+
+export default AuthService;
