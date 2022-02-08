078aa27fc0649f04270ae7e2d7d233cd9f0a3bbe
Yu Jin Jang <jyooj08@naver.com>
2022-02-08 10:00:29 +0900

    Set env file

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signO
 class AuthService{
     constructor(){
         this.firebaseConfig = {
-            apiKey: "AIzaSyCDNu1QRNk27AhmH4E6-S-afBfyxWFh9_g",
-            authDomain: "haru-diary-bda69.firebaseapp.com",
-            projectId: "haru-diary-bda69",
-            storageBucket: "haru-diary-bda69.appspot.com",
-            messagingSenderId: "93188198611",
-            appId: "1:93188198611:web:23c0128d6ff394b593c426"
+            apiKey: process.env.REACT_APP_API_KEY,
+            authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+            projectId: process.env.REACT_APP_PROJECT_ID,
+            storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+            messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+            appId: process.env.REACT_APP_APP_ID
           };
 
           this.app = initializeApp(this.firebaseConfig);
